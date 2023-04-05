a12afe4c536106882dc8fcdd57f2ab1fc7308013
Deepak-nishad <deepakkumar62559@gmail.com>
2023-04-05 12:13:25 +0530

    create a new user

--- React Project/inote/backend/index.js
+++ React Project/inote/backend/index.js
@@ -1,7 +1,7 @@
 const connectToMongo = require('./db');
 const express = require('express');
 const app = express()
-const port = 3000
+const port = 5000 || Process.ENV.PORT
 connectToMongo();
 
 
@@ -11,5 +11,5 @@ app.use('/api/auth', require('./routes/auth'))
 // app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
