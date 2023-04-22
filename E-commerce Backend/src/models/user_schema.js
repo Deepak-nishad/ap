b6e6b439bda3c8e61642c845dc48842bab2aa429
Deepak-nishad <deepakkumar62559@gmail.com>
2023-04-22 21:13:48 +0530

    user scahema pre hooks and method used

--- E-commerce Backend/src/models/user_schema.js
+++ E-commerce Backend/src/models/user_schema.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import AuthRole from "../utils/authRoles.js";
+
+import bcrypt from "bcryptjs"
 const userSchema = new mongoose.Schema(
     {
     
@@ -29,5 +31,19 @@ const userSchema = new mongoose.Schema(
 {timestamps: true}
 )
 
+// Encyption the password before saving
+
+userSchema.pre("save" , async function(next){
+       if(!this.isModified("password")) return next()
+       this.password = await bcrypt.hash(this.password, 10)
+       next()
+})
+
+userSchema.method = {
+  //compare password
+  comparePassword: async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+  }
+}
 export default mongoose.model("User", userSchema)
 
