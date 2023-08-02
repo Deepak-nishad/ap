9d4e5c1a5ddfa425a78668b593de141f58c5fa3b
Deepak-nishad <deepakkumar62559@gmail.com>
2023-08-03 01:03:09 +0530

    Working on context api

---
+++ Practics Work/react_self_try/context-api/src/probelm.js
@@ -0,0 +1,32 @@
+// import React, { useState } from "react";
+
+// const GrandChild = (props) => {
+//   return (
+//     <div>
+//       <h3>grandChild </h3>
+//       <Child brand={props.brand} />
+//     </div>
+//   );
+// };
+
+// const Child = (props) => {
+//   return (
+//     <div>
+//       <h2>Child :{props.brand} </h2>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const [brandname] = useState("Amazone");
+//   return (
+//     <div>
+//       <h1>hloo</h1>
+
+//       {<GrandChild brand={brandname} />}
+//     </div>
+//   );
+// };
+
+// export default App;
+// //
