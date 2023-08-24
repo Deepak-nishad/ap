9cb58904e11789a8aaac4471128c6096de43f8d4
Deepak-nishad <deepakkumar62559@gmail.com>
2023-08-24 10:57:51 +0530

    dom assignemnt 9 complete

---
+++ DOM Assignments 2.0 Batch copy/DOM P9/DOM P9/index.js
@@ -0,0 +1,9 @@
+// Get the <h1> element
+var h1Element = document.querySelector(".caption .title");
+
+// Change the color to red
+h1Element.style.color = "red";
+
+var button = document.querySelector(".add-to-cart");
+
+button.style.backgroundColor = " #ff0066";
